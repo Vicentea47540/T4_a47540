276b3e069bd7f94af181cb6419cc94d715392207
Vicentea47540 <104469598+Vicentea47540@users.noreply.github.com>
2022-12-05 21:28:43 +0000

    Actions

--- Assets/GAction.cs
+++ Assets/GAction.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GAction : MonoBehaviour
+using UnityEngine.AI;
+public abstract class GAction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string actionName = "Action";
+    public float cost = 1.0f;
+    public GameObject target;
+    public GameObject targetTag;
+    public float duration = 0;
+    public WorldStates[] preConditions;
+    public WorldStates[] afterEffects;
+    public NavMeshAgent agent;
+    public Dictionary<string, int> preconditions;
+    public Dictionary<string, int> effects;
+    public GAgent agentBelifs;
+    public bool running = false;
+    public GAction ()
     {
-        
+        preconditions = new Dictionary<string, int>();
+        effects = new Dictionary<string, int>();
     }
+    public void Awake()
+    {
+        agent = this.gameObject.GetComponent<NavMeshAgent>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (preConditions != null)
+        {
+            foreach (WorldStates w in preConditions)
+            {
+                preconditions.Add(w.key, w.value);
+            }
+        }
+        if (afterEffects != null)
+        {
+            foreach (WorldStates w in afterEffects)
+            {
+                effects.Add(w.key, w.value);
+            }
+        }
     }
+    public bool IsAchievable()
+    {
+        bool possible = true;
+
+    }
+    public bool IsAchievableGiven(Dictionary<string, int> conditions)
+    {
+        bool possible = true;
+        foreach (KeyValuePair<string, int> p in preconditions)
+        {
+            if (!conditions.ContainsKey(p.Key))
+            {
+                possible = false;
+            }
+        }
+        return true;
+    }
+    public abstract bool PrePerform();
+    public abstract bool PostPerform();
+
+
 }
