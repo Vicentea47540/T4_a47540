6ebead3a98128d3e50d8f5a472aa0ec4359c59c6
Vicentea47540 <104469598+Vicentea47540@users.noreply.github.com>
2022-12-05 22:01:51 +0000

    Fix

--- Assets/GAction.cs
+++ Assets/GAction.cs
@@ -1,7 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+
 public abstract class GAction : MonoBehaviour
 {
     public string actionName = "Action";
@@ -9,56 +10,56 @@ public abstract class GAction : MonoBehaviour
     public GameObject target;
     public GameObject targetTag;
     public float duration = 0;
-    public WorldStates[] preConditions;
-    public WorldStates[] afterEffects;
+    public WorldState[] preConditions;
+    public WorldState[] afterEffects;
     public NavMeshAgent agent;
+
     public Dictionary<string, int> preconditions;
     public Dictionary<string, int> effects;
-    public GAgent agentBelifs;
+
+    public WorldStates agentBeliefs;
+
     public bool running = false;
-    public GAction ()
+
+    public GAction()
     {
         preconditions = new Dictionary<string, int>();
         effects = new Dictionary<string, int>();
     }
+
     public void Awake()
     {
         agent = this.gameObject.GetComponent<NavMeshAgent>();
 
         if (preConditions != null)
-        {
-            foreach (WorldStates w in preConditions)
+            foreach (WorldState w in preConditions)
             {
                 preconditions.Add(w.key, w.value);
             }
-        }
+
         if (afterEffects != null)
-        {
-            foreach (WorldStates w in afterEffects)
+            foreach (WorldState w in afterEffects)
             {
                 effects.Add(w.key, w.value);
             }
-        }
+            
     }
+
     public bool IsAchievable()
     {
-        bool possible = true;
-
+        return true;
     }
+
     public bool IsAchievableGiven(Dictionary<string, int> conditions)
     {
-        bool possible = true;
         foreach (KeyValuePair<string, int> p in preconditions)
         {
             if (!conditions.ContainsKey(p.Key))
-            {
-                possible = false;
-            }
+                return false;
         }
         return true;
     }
+
     public abstract bool PrePerform();
     public abstract bool PostPerform();
-
-
 }
